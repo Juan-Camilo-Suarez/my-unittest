62f5a22f402f1e4db0483472bf7bdc72564b49a4
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-09-30 22:47:17 +0300

    create exception in calculator

--- src/main_4.py
+++ src/main_4.py
@@ -3,11 +3,17 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("both arguments should be numbers")
+        if y == 0:
+            raise ZeroDivisionError("cannot divide by zero")
         return x / y
 
     def add(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("both arguments should be numbers")
         return x + y
 
 
 if __name__ == '__main__':
-    calculator = Calculator().divide(6,3 )
+    calculator = Calculator().divide(6, 3)
