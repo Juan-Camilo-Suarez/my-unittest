53ca1a815b459c7bfb825ff24113c7c662e80d16
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-09-30 22:48:07 +0300

    test to cach exceptions

--- test/test_main4.py
+++ test/test_main4.py
@@ -1,4 +1,5 @@
 import pytest
+from contextlib import nullcontext as does_not_raise
 
 from src.main_4 import Calculator
 
@@ -6,17 +7,22 @@ from src.main_4 import Calculator
 class TestCalculator:
     # test with parametrizer
     @pytest.mark.parametrize(
-        "x,y,res",
-        [(1, 2, 0.5),
-         (5, -1, -5),
+        "x,y,res,expectation",
+        [(1, 2, 0.5, does_not_raise()),
+         (5, -1, -5, does_not_raise()),
+         (5, "-1", -5, pytest.raises(TypeError)),
+         (5, 0, -5, pytest.raises(ZeroDivisionError)),
          ])
-    def test_divide(self, x, y, res):
-        assert Calculator().divide(x, y) == res
+    def test_divide(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
 
     @pytest.mark.parametrize(
-        "x,y,res",
-        [(1, 2, 3),
-         (5, -1, 4),
+        "x,y,res,expectation",
+        [(1, 2, 3, does_not_raise()),
+         (5, -1, 4, does_not_raise()),
+         (5, "-1", -5, pytest.raises(TypeError)),
          ])
-    def test_add(self, x, y, res):
-        assert Calculator().add(x, y) == res
+    def test_add(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().add(x, y) == res
