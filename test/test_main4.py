c881494e61b3c1f5a7dfcac6dced32df542a3dd0
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2023-09-30 18:35:34 +0300

    create test to clase calculator with parametrizer

---
+++ test/test_main4.py
@@ -0,0 +1,22 @@
+import pytest
+
+from src.main_4 import Calculator
+
+
+# test with parametrizer
+@pytest.mark.parametrize(
+    "x,y,res",
+    [(1, 2, 0.5),
+     (5, -1, -5),
+     ])
+def test_divide(x, y, res):
+    assert Calculator().divide(x, y) == res
+
+
+@pytest.mark.parametrize(
+    "x,y,res",
+    [(1, 2, 3),
+     (5, -1, 4),
+     ])
+def test_add(x, y, res):
+    assert Calculator().add(x, y) == res
